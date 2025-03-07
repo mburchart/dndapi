2e8faae5f40940996af7597c0d0cd47c7fb3d07c
Marc Burchart <marc.burchart@fernuni-hagen.de>
2025-03-07 01:37:10 +0100

    added sql struct for users

--- postgres/psql_dump.sql
+++ postgres/psql_dump.sql
@@ -1,3 +1,8 @@
-/*CREATE TABLE placeholder (
+CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-);*/
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    pw CHAR(128) NOT NULL,
+    salt CHAR(128) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
